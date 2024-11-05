c3aac584b509a8243fe43aeabef85a3607761fc2
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-11-05 16:44:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,10 @@ pdf = pd.read_csv(output)
 if pdf.empty:
     st.warning("The DataFrame is empty after loading!")
 else:
-    st.success("Data loaded successfully!")
+    # st.success("Data loaded successfully!")
     pdf['legal_ball'] = pdf.apply(lambda row: 1 if row['outcome'] in ['no run', 'out', 'four', 'run', 'six', 'leg bye', 'bye'] else 0, axis=1)
-    st.write(f"DataFrame shape: {pdf.shape}")  # Display the shape of the DataFrame
-    st.write(pdf.tail())  # Display the first few rows of the DataFrame
+    # st.write(f"DataFrame shape: {pdf.shape}")  # Display the shape of the DataFrame
+    # st.write(pdf.tail())  # Display the first few rows of the DataFrame
 bpdf=pdf
 idf = pd.read_csv("Datasets/lifesaver_bat_tests.csv",low_memory=False)
 bidf = pd.read_csv("Datasets/lifesaver_bowl_tests.csv",low_memory=False)
@@ -33,3 +33,54 @@ sidebar_option = st.sidebar.radio(
     "Select an option:",
     ("Player Profile", "Matchup Analysis","Strength vs Weakness","Match by Match Analysis")
 )
+if sidebar_option == "Player Profile":
+    st.header("Player Profile")
+
+    # Player search input (selectbox)
+    player_name = st.selectbox("Search for a player", idf['batsman'].unique())
+
+    # Filter the data for the selected player
+    player_info = idf[idf['batsman'] == player_name].iloc[0]
+
+    # Check if the player exists in info_df
+    matching_rows = info_df[info_df['Player_name'] == player_name]
+
+    if not matching_rows.empty:
+        # If there is a matching row, access the first one
+        p_info = matching_rows.iloc[0]
+    else:
+        # st.write(f"No player found with the name '{player_name}'")
+        p_info = None  # Set a fallback
+
+    # Tabs for "Overview", "Career Statistics", and "Current Form"
+    tab1, tab2 = st.tabs(["Career Statistics", "Current Form"])
+    with tab2:
+            st.header("Career Statistics")
+    
+            # Dropdown for Batting or Bowling selection
+            option = st.selectbox("Select Career Stat Type", ("Batting", "Bowling"))
+    
+            # Show Career Averages based on the dropdown
+            st.subheader("Career Performance")
+    
+            # Display Career Averages based on selection
+            if option == "Batting":
+                # Create a temporary DataFrame and filter the player's row
+                temp_df = idf.drop(columns=['final_year','matches'])
+                player_stats = temp_df[temp_df['batsman'] == player_name]  # Filter for the selected player
+    
+                # Convert column names to uppercase and replace underscores with spaces
+                player_stats.columns = [col.upper().replace('_', ' ') for col in player_stats.columns]
+                player_stats=round_up_floats(player_stats)
+                # Display the player's statistics in a table format with bold headers
+                st.markdown("### Batting Statistics")
+                columns_to_convert = ['RUNS','HUNDREDS', 'FIFTIES','THIRTIES', 'HIGHEST SCORE']
+    
+                   # Fill NaN values with 0
+                player_stats[columns_to_convert] = player_stats[columns_to_convert].fillna(0)
+                    
+                   # Convert the specified columns to integer type
+                player_stats[columns_to_convert] = player_stats[columns_to_convert].astype(int)
+                # player_stats=player_stats.drop(columns={'UNNAMED:0'})
+                st.table(player_stats.style.set_table_attributes("style='font-weight: bold;'"))                
+                # Initializing an empty DataFrame for results and a counter
